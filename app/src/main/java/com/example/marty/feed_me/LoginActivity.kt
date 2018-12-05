84322e927a81ab2b1d068c638f565477b91f3d51
toneymar <35696013+toneymar@users.noreply.github.com>
2018-12-05 21:59:51 +0100

    firebase login working

--- app/src/main/java/com/example/marty/feed_me/LoginActivity.kt
+++ app/src/main/java/com/example/marty/feed_me/LoginActivity.kt
@@ -3,6 +3,10 @@ package com.example.marty.feed_me
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -10,11 +14,52 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
-        btnLogin.setOnClickListener {
-            val intent = Intent()
-            intent.setClass(this@LoginActivity, MainActivity::class.java)
-            startActivity(intent)
+    fun loginClick(v : View) {
+        if (!isFormValid())
+            return
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                etEmail.text.toString(), etPassword.text.toString()
+        ).addOnSuccessListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }.addOnFailureListener {
+            Toast.makeText(this@LoginActivity, "Login error ${it.message}", Toast.LENGTH_LONG).show()
         }
     }
+
+    fun registerClick(v: View) {
+        if (!isFormValid())
+            return
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                etEmail.text.toString(), etPassword.text.toString()
+        ).addOnSuccessListener {
+            val user = it.user
+            user.updateProfile(
+                    UserProfileChangeRequest.Builder().
+                            setDisplayName(userNameFromEmail(user.email!!)).build()
+            )
+            Toast.makeText(this@LoginActivity, "Registration ok!", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this@LoginActivity, "Register error ${it.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isFormValid(): Boolean {
+        return when {
+            etEmail.text.isEmpty() -> {
+                etEmail.error = "This field can not be empty"
+                false
+            }
+            etPassword.text.isEmpty() -> {
+                etPassword.error = "This field can not be empty"
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun userNameFromEmail(email: String) = email.substringBefore("@")
 }
