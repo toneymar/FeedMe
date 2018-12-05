811417d6123c2530b3f7ad6e41fabdb00e9be329
toneymar <35696013+toneymar@users.noreply.github.com>
2018-12-05 20:58:16 +0100

    login layout finished

--- app/src/main/java/com/example/marty/feed_me/LoginActivity.kt
+++ app/src/main/java/com/example/marty/feed_me/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.marty.feed_me
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent()
+        }
     }
 }
