d8e00a1937dee3e1fd86144a74c75018b7f17250
Pratik Karki <karkipra@grinnell.edu>
2018-12-06 18:35:14 +0100

    added delimiter to API

--- app/src/main/java/com/example/marty/feed_me/network/RecipeAPI.kt
+++ app/src/main/java/com/example/marty/feed_me/network/RecipeAPI.kt
@@ -1,9 +1,16 @@
 package com.example.marty.feed_me.network
 
+import com.example.marty.feed_me.data.RecipeResult
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+//https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=1
 
 interface RecipeAPI{
 
-    @GET("/search")
-    fun //need to fill out
+    @GET("/api/search")
+    fun getRecipe(@Query("key") key : String,
+                   @Query("q") q : String,
+                   @Query("page") page : Int) : Call<RecipeResult>
 }
